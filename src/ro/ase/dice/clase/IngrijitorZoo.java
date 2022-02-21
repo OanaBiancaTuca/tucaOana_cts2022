8e74faec6ebb6a34a02039c407de13a25b0cea0b
OanaBiancaTuca <tucaoana19@stud.ase.ro>
2022-02-21 14:58:34 +0200

    Initial commit

---
+++ src/ro/ase/dice/clase/IngrijitorZoo.java
@@ -0,0 +1,20 @@
+package ro.ase.dice.clase;
+
+public class IngrijitorZoo {
+    private String nume;
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public IngrijitorZoo(String nume) {
+        this.nume = nume;
+    }
+    public void hranesteAnimal(Animal animal,String mancare){
+        animal.mananca(mancare);
+    }
+}
